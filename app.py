6300151b8211c3a3fd11a499dcd4d81a2b52eb06
AmeyGaikwad <ameygaikwad89@gmail.com>
2024-06-14 02:49:44 +0530

    Commiting v1 of the app

---
+++ app.py
@@ -0,0 +1,61 @@
+import sys
+import os
+import json
+import glob
+import re
+import pandas as pd
+
+
+def get_column_names(schemas, ds_name, sorting_key='column_position'):
+    column_details = schemas[ds_name]
+    columns = sorted(column_details, key=lambda col: col[sorting_key])
+    return [col['column_name'] for col in columns]
+
+
+def read_csv(file, schemas):
+    file_path_list = re.split('[/\\\]', file)
+    ds_name = file_path_list[-2]
+
+    columns = get_column_names(schemas, ds_name)
+    df = pd.read_csv(file, names=columns)
+    return df
+
+
+def to_json(df, tgt_base_dir, ds_name, file_name):
+    json_file_path = f'{tgt_base_dir}/{ds_name}/{file_name}'
+    os.makedirs(f'{tgt_base_dir}/{ds_name}', exist_ok=True)
+    df.to_json(json_file_path,
+               orient='records',
+               lines=True)
+    print(f'File Created at {json_file_path} \n')
+
+
+def file_converter(src_base_dir, tgt_base_dir, ds_name, schemas):
+    files = glob.glob(f'{src_base_dir}/{ds_name}/part-*')
+    for file in files:
+        print(f'Processing {file}')
+        df = read_csv(file, schemas)
+        file_name = re.split('[/\\\]', file)[-1]
+        to_json(df, tgt_base_dir, ds_name, file_name)
+
+
+def process_file(ds_names=None):
+    src_base_dir = os.environ.get('SRC_BASE_DIR')
+    tgt_base_dir = os.environ.get('TGT_BASE_DIR')
+    # src_base_dir = 'data/retail_db'
+    # tgt_base_dir = 'data/retail_db_json'
+    schemas = json.load(open(f'{src_base_dir}/schemas.json'))
+
+    if not ds_names:
+        ds_names = schemas.keys()
+    for ds_name in ds_names:
+        print(f'Processing {ds_name}')
+        file_converter(src_base_dir, tgt_base_dir, ds_name, schemas)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        ds_names = json.loads(sys.argv[1])
+        process_file(ds_names)
+    else:
+        process_file()
---
+++ hy.py
@@ -0,0 +1,13 @@
+import sys
+import os
+args = sys.argv
+
+print(args[1])
+
+# file_name = args[0]
+# arg = args[1]
+
+# print(f'Hello {arg} from {file_name}')
+# host = os.environ.get('HOST')
+# print(f'Connecting to {host}')
+      
